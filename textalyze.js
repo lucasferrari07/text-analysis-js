e64e7662b8bbf3c79b6d2b989c124ec811c777ce
Lucas Ferrari da Costa <lucas.ferrari.costa@gmail.com>
2019-03-27 21:25:46 -0300

    Implement suggested changes for PR #1
    - Changed the array.forEach approach for the array.reduce one, as it's now a single call.
    - Isolated the execution part of the file so it runs only if the module is the main module.

--- textalyze.js
+++ textalyze.js
@@ -1,22 +1,23 @@
 /**
  * Given an input Array, returns a Map containing the count of each item in the input.
  * @param {Array} array - The array of items to count
- * @param {Map} counts - A Map containing the counts of the items in the input array
+ * @returns {Map} counts - A Map containing the counts of the items in the input array
  */
 function itemCounts(array) {
-  let counts = new Map();
-
-  for (const a of array) {
-    counts.has(a) ? counts.set(a, counts.get(a) + 1) : counts.set(a, 1);
-  }
-
-  return counts;
+  return array.reduce((map, a) => {
+    if (map.has(a)) {
+      map.set(a, map.get(a) + 1);
+    } else {
+      map.set(a, 1);
+    }
+    return map;
+  }, new Map());
 }
 
-const elements = ["a", "a", "a", "b", "b", "c"];
-console.log(`Count for [${elements}] is...`);
-itemCounts(elements).forEach((count, elem) => {
-  console.log(elem, count);
-});
+if (require.main === module) {
+  const elements = ['a', 'a', 'a', 'b', 'b', 'c'];
+  console.log(`Count for [${elements}] is...`);
+  console.table(itemCounts(elements));
+}
 
 module.exports = { itemCounts };
